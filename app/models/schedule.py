10384ce7a7a1f1839a8de1c06fc14bf961aa86ee
shirshachar <shirshachar17@gmail.com>
2024-11-19 16:41:48 -0500

    fix db table name

--- app/models/schedule.py
+++ app/models/schedule.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, DateTime
 from app.database import Base
 
 class Schedule(Base):
-    __tablename__ = 'schedules'
+    __tablename__ = 'schedules2'
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, nullable=False)

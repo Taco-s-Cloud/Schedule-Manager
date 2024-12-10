4089cb6d3304ee7cc660824f40670f0350241368
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-12-10 15:30:34 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 from .routers import schedules
 from .database import engine, Base
+from .middleware.logging import setup_logging, add_correlation_id
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+    setup_logging()
+    add_correlation_id(app)
     # Register blueprints
     app.register_blueprint(schedules.schedules_blueprint)
     # Create tables if they don't exist
     Base.metadata.create_all(bind=engine)
-
     return app

7bb090efb4389f4e722199144b396dc03d011e30
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-12-10 17:22:29 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ from .middleware.logging import setup_logging, add_correlation_id
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    setup_logging()
-    add_correlation_id(app)
+    # setup_logging()
+    # add_correlation_id(app)
     # Register blueprints
     app.register_blueprint(schedules.schedules_blueprint)
     # Create tables if they don't exist

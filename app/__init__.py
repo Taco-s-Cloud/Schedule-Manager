7b593765fff0743362164f11eee7300a8369c629
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-12-10 22:12:57 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ from flask_cors import CORS
 from .routers import schedules
 from .database import engine, Base
 # from .middleware.logging import setup_logging, add_correlation_id
+from google.cloud import logging_v2 as cloud_logging
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+    client = cloud_logging.Client()
+    client.setup_logging() 
     # setup_logging()
     # add_correlation_id(app)
     # Register blueprints
